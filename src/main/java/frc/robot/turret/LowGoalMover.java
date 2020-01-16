17dcb7861594ca0ebd2d4bbcc05c28a9237c8652
Dylan <40635921+robothomas@users.noreply.github.com>
2020-01-16 12:51:38 -0600

    Rename folder and package to turret

--- src/main/java/frc/robot/turret/LowGoalMover.java
+++ src/main/java/frc/robot/turret/LowGoalMover.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.goalMover;
+package frc.robot.turret;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
