d164c82145163e4e0b1a08fd1439374416887a93
Dylan <robothomas@users.noreply.github.com>
2020-01-18 19:32:04 -0600

    Added comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -71,41 +71,45 @@ public final class Constants {
   // Constant Speeds
   public static final double armPercentSpeed = 0.4;
   
-  public static final double shooterRPM = 10;
+  public static final double shooterRPM = 30;
+  public static final double shooterError = 20;
   
-  public static class DriveFeedforward {
-    public static final double ks = 0;
-    public static final double kv = 0;
-    public static final double ka = 0;
-  }
-    
+  // PID Constants
   public static class leftDrive {
-    public static final double Kp = 0.1;
+    public static double Kp = 0.1;
     public static final double Ki = 0;
     public static final double Kd = 0;
   }
 
   public static class rightDrive {
-    public static final double Kp = 0.1;
-    public static final double Ki = 0;
-    public static final double Kd = 0;
+    public static double Kp = 0.1;
+    public static double Ki = 0;
+    public static double Kd = 0;
   }
 
+  public static class shooterLeftPID {
+    public static double Kp = 0.1;
+    public static double Ki = 0;
+    public static double Kd = 0;
+  }
+
+  public static class shooterRightPID {
+    public static double Kp = 0.1;
+    public static double Ki = 0;
+    public static double Kd = 0;
+  }
+
+  // Ramsete controller constants
   public static class Ramsete {
     public static final double kb = 2;
     public static final double kzeta = 0.7;
   }
 
-  public static class shooterLeftPID {
-    public static final double Kp = 0.1;
-    public static final double Ki = 0;
-    public static final double Kd = 0;
-  }
-
-  public static class shooterRightPID {
-    public static final double Kp = 0.1;
-    public static final double Ki = 0;
-    public static final double Kd = 0;
+  // Feed Forward Constants
+  public static class DriveFeedforward {
+    public static final double ks = 0;
+    public static final double kv = 0;
+    public static final double ka = 0;
   }
   
 }
