81bd627b97e95216541376eac9aa005ba2c05949
colter <colter_clement@isd31.net>
2020-01-21 15:55:59 -0800

    Add reference template

--- src/main/java/frc/robot/wheel/Spinner.java
+++ src/main/java/frc/robot/wheel/Spinner.java
@@ -17,6 +17,7 @@ import com.revrobotics.ControlType;
 
 import edu.wpi.first.wpilibj.I2C;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.platform.DeviceType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -38,53 +39,22 @@ private static final AlternateEncoderType kAltEncType = AlternateEncoderType.kQu
 private static final int kCPR = 8192;
 
 private CANSparkMax m_motor;
-private CANPIDController m_pidController;
 public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
-
-private CANEncoder m_alternateEncoder;
-
-CANSparkMax SpinnerMotor = new CANSparkMax(kSpinnerPort, MotorType.kBrushless);
+private final CANSparkMax SpinnerMotor = new CANSparkMax(kSpinnerPort, MotorType.kBrushless);
+  private final CANPIDController spinController = SpinnerMotor.getPIDController();
 
 
 public Spinner() {
-  
+  spinController.setP(shooterLeftPID.Kp);
+  spinController.setI(shooterLeftPID.Ki);
+  spinController.setD(shooterLeftPID.Kd); 
 }
 
 
 public void spin(){
 
-  m_motor = new CANSparkMax(kCanID, kMotorType);
-  m_alternateEncoder = m_motor.getAlternateEncoder(kAltEncType, kCPR);
-
-  m_motor.restoreFactoryDefaults();
-  m_pidController = m_motor.getPIDController();
-  m_pidController.setFeedbackDevice(m_alternateEncoder);
-
-
-  kP = 0.1; 
-  kI = 1e-4;
-  kD = 1; 
-  kIz = 0; 
-  kFF = 0; 
-  kMaxOutput = 1; 
-  kMinOutput = -1;
-
-  m_pidController.setP(kP);
-  m_pidController.setI(kI);
-  m_pidController.setD(kD);
-  m_pidController.setIZone(kIz);
-  m_pidController.setFF(kFF);
-  m_pidController.setOutputRange(kMinOutput, kMaxOutput);
-
-  SmartDashboard.putNumber("P Gain", kP);
-  SmartDashboard.putNumber("I Gain", kI);
-  SmartDashboard.putNumber("D Gain", kD);
-  SmartDashboard.putNumber("I Zone", kIz);
-  SmartDashboard.putNumber("Feed Forward", kFF);
-  SmartDashboard.putNumber("Max Output", kMaxOutput);
-  SmartDashboard.putNumber("Min Output", kMinOutput);
-  SmartDashboard.putNumber("Set Rotations", 0);
+  //spinController.setReference(10, ControlMode.Position);  
 
 
 }
@@ -93,29 +63,5 @@ public void spin(){
 @Override
 public void periodic() {
 
-  double p = SmartDashboard.getNumber("P Gain", 0);
-  double i = SmartDashboard.getNumber("I Gain", 0);
-  double d = SmartDashboard.getNumber("D Gain", 0);
-  double iz = SmartDashboard.getNumber("I Zone", 0);
-  double ff = SmartDashboard.getNumber("Feed Forward", 0);
-  double max = SmartDashboard.getNumber("Max Output", 0);
-  double min = SmartDashboard.getNumber("Min Output", 0);
-  double rotations = SmartDashboard.getNumber("Set Rotations", 0);
 
-if((p != kP)) { m_pidController.setP(p); kP = p; }
-    if((i != kI)) { m_pidController.setI(i); kI = i; }
-    if((d != kD)) { m_pidController.setD(d); kD = d; }
-    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
-    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
-    if((max != kMaxOutput) || (min != kMinOutput)) { 
-      m_pidController.setOutputRange(min, max); 
-      kMinOutput = min; kMaxOutput = max; 
-    }
-
-    m_pidController.setReference(rotations, ControlType.kPosition);
-    
-    SmartDashboard.putNumber("SetPoint", rotations);
-    SmartDashboard.putNumber("ProcessVariable", m_alternateEncoder.getPosition());
-
-}
 }
